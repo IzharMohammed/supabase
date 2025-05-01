3701366e04dd36ca570ac01f993f0cbd4b95c2f7
MohammedIzhar77 <izharmohammed21@gmail.com>
2025-05-01 21:05:29 +0530

    Added ui

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -1,3 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient();
+export const supabase = createClient(
+    import.meta.env.VITE_SUPABASE_KEY || "",
+    import.meta.env.VITE_SUPABASE_URL || ""
+);
+
